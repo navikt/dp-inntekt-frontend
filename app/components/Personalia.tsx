f3ee034c127ac941a178e47b35c7dde9e1cd539d
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-04-09 15:01:24 +0200

    Legge til copy personnummer knapp

--- app/components/Personalia.tsx
+++ app/components/Personalia.tsx
@@ -1,4 +1,5 @@
-import { BodyShort, Box, Button, HStack, Spacer } from "@navikt/ds-react";
+import { ArrowsCirclepathIcon } from "@navikt/aksel-icons";
+import { BodyShort, Box, Button, CopyButton, HStack, Spacer } from "@navikt/ds-react";
 import { KvinneIkon } from "./Ikoner/KvinneIkon";
 
 interface IProps {
@@ -12,15 +13,21 @@ export function Personalia({ navn, pnr, sistOppdatert }: IProps) {
     <Box background="surface-default" padding="6" borderRadius="xlarge" borderColor="border-subtle">
       <HStack gap="4" wrap={false} align="center">
         <KvinneIkon />
-        <HStack gap="4">
+        <HStack gap="4" align="center">
           <BodyShort weight="semibold">{navn}</BodyShort>
           <BodyShort>/</BodyShort>
           <BodyShort>{pnr}</BodyShort>
+          <CopyButton copyText={pnr} />
         </HStack>
         <Spacer />
         <HStack gap="4" align="center">
           <BodyShort size="small">{`Sist oppdatert: ${sistOppdatert}`}</BodyShort>
-          <Button variant="secondary" size="small">
+          <Button
+            variant="secondary"
+            size="small"
+            icon={<ArrowsCirclepathIcon title="star" fontSize="1.5rem" />}
+            iconPosition="right"
+          >
             Hent inntekter på nytt
           </Button>
         </HStack>
