336da44a70fa42a04e08aedb7a0fb26d47130638
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-05-07 14:33:16 +0200

    Oppdatert mock data

--- app/components/InntektPerioder/InntektPerioder.tsx
+++ app/components/InntektPerioder/InntektPerioder.tsx
@@ -21,23 +21,6 @@ export function InntektPerioder() {
 
 const inntekterPropsMockData = [
   {
-    ar: "2020",
-    maneder: [
-      { maned: "Januar", inntekt: 10000 },
-      { maned: "Februar", inntekt: 12000 },
-      { maned: "Mars", inntekt: 11000 },
-      { maned: "April", inntekt: 13000 },
-      { maned: "Mai", inntekt: 0 },
-      { maned: "Juni", inntekt: 0 },
-      { maned: "Juli", inntekt: 0 },
-      { maned: "August", inntekt: 0 },
-      { maned: "September", inntekt: 0 },
-      { maned: "Oktober", inntekt: 0 },
-      { maned: "November", inntekt: 0 },
-      { maned: "Desember", inntekt: 0 },
-    ],
-  },
-  {
     ar: "2021",
     maneder: [
       { maned: "Januar", inntekt: 10000 },
@@ -86,6 +69,23 @@ const inntekterPropsMockData = [
       { maned: "Oktober", inntekt: 19000 },
       { maned: "November", inntekt: 20000 },
       { maned: "Desember", inntekt: 21000 },
+    ],
+  },
+  {
+    ar: "2024",
+    maneder: [
+      { maned: "Januar", inntekt: 10000 },
+      { maned: "Februar", inntekt: 12000 },
+      { maned: "Mars", inntekt: 11000 },
+      { maned: "April", inntekt: 13000 },
+      { maned: "Mai", inntekt: 0 },
+      { maned: "Juni", inntekt: 0 },
+      { maned: "Juli", inntekt: 0 },
+      { maned: "August", inntekt: 0 },
+      { maned: "September", inntekt: 0 },
+      { maned: "Oktober", inntekt: 0 },
+      { maned: "November", inntekt: 0 },
+      { maned: "Desember", inntekt: 0 },
     ],
   },
 ];
