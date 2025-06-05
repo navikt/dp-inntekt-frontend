ba60eba5ea8164ccb4c405b9adb3984ab95cf9ec
Arselan.Sultani <arselan.sultani@nav.no>
2025-06-05 17:49:29 +0200

    rename contextvirsomheter til contextVirksomheter og la til slett av inntektkilde

--- app/components/Virksomhet.tsx
+++ app/components/Virksomhet.tsx
@@ -1,63 +1,69 @@
-import { Button, ExpansionCard, VStack } from "@navikt/ds-react";
+import {PlusCircleIcon, TrashIcon} from "@navikt/aksel-icons";
+import {Button, ExpansionCard, VStack} from "@navikt/ds-react";
 import VirsomhetInntekter from "~/components/VirsomhetInntekter";
 import type { IPeriode, IVirksomhet } from "~/types/inntekt.types";
 import { formaterNorskDato, formatterNorskTall } from "~/utils/formattering.util";
 import { erPersonnummer, maskerePersonnummer } from "~/utils/generell.util";
 
 interface IProps {
-  virksomhet: IVirksomhet;
-  inntektsPeriode: IPeriode;
+    virksomhet: IVirksomhet,
+    inntektsPeriode: IPeriode,
+    slettVirksomhet: (virksomhetsnummer: string) => void
 }
 
 interface IInntekInfo {
-  overskrift: string;
-  verdi: string;
+    overskrift: string;
+    verdi: string;
 }
 
-export function InntektInfo({ overskrift, verdi }: IInntekInfo) {
-  return (
-    <VStack gap="0">
-      <strong>{overskrift}</strong>
-      <p>{verdi}</p>
-    </VStack>
-  );
+export function InntektInfo({overskrift, verdi}: IInntekInfo) {
+    return (
+        <VStack gap="0">
+            <strong>{overskrift}</strong>
+            <p>{verdi}</p>
+        </VStack>
+    );
 }
 
-export default function Virksomhet({ virksomhet, inntektsPeriode }: IProps) {
-  const { virksomhetsnummer, virksomhetsnavn, periode, totalBelop } = virksomhet;
-  const erPrivatPerson = erPersonnummer(virksomhetsnummer);
+export default function Virksomhet({virksomhet, inntektsPeriode, slettVirksomhet}: IProps) {
+    const {virksomhetsnummer, virksomhetsnavn, periode, totalBelop} = virksomhet;
 
-  return (
-    <ExpansionCard aria-label={`Inntekt for ${virksomhetsnummer}`}>
-      <ExpansionCard.Header>
-        <ExpansionCard.Title>
-          {erPrivatPerson ? "Privatperson" : virksomhetsnavn}
-        </ExpansionCard.Title>
-        <ExpansionCard.Description>
-          <VStack gap="4">
-            <InntektInfo
-              overskrift={erPrivatPerson ? "Personnummer" : "Organisasjonsnummer"}
-              verdi={erPrivatPerson ? maskerePersonnummer(virksomhetsnummer) : virksomhetsnummer}
-            />
-            <InntektInfo
-              overskrift="Periode"
-              verdi={`${formaterNorskDato(periode.fraOgMed)} - ${formaterNorskDato(
-                periode.tilOgMed
-              )}`}
-            />
-            <InntektInfo
-              overskrift="Beløp for perioden"
-              verdi={formatterNorskTall(Number(totalBelop))}
-            />
-          </VStack>
-        </ExpansionCard.Description>
-      </ExpansionCard.Header>
-      <ExpansionCard.Content>
-        <VirsomhetInntekter virksomhet={virksomhet} inntektsPeriode={inntektsPeriode} />
-        <Button size="small" className="mt-4">
-          Legg til inntekt
-        </Button>
-      </ExpansionCard.Content>
-    </ExpansionCard>
-  );
+    const erPrivatPerson = erPersonnummer(virksomhetsnummer);
+
+    return (
+        <ExpansionCard aria-label={`Inntekt for ${virksomhetsnummer}`}>
+            <ExpansionCard.Header>
+                <ExpansionCard.Title>
+                    {erPrivatPerson ? "Privatperson" : virksomhetsnavn}
+                </ExpansionCard.Title>
+                <ExpansionCard.Description>
+                    <VStack gap="4">
+                        <InntektInfo
+                            overskrift={erPrivatPerson ? "Personnummer" : "Organisasjonsnummer"}
+                            verdi={erPrivatPerson ? maskerePersonnummer(virksomhetsnummer) : virksomhetsnummer}
+                        />
+                        <InntektInfo
+                            overskrift="Periode"
+                            verdi={`${formaterNorskDato(periode.fraOgMed)} - ${formaterNorskDato(
+                                periode.tilOgMed
+                            )}`}
+                        />
+                        <InntektInfo
+                            overskrift="Beløp for perioden"
+                            verdi={formatterNorskTall(Number(totalBelop))}
+                        />
+                    </VStack>
+                </ExpansionCard.Description>
+            </ExpansionCard.Header>
+            <ExpansionCard.Content>
+                <VirsomhetInntekter virksomhet={virksomhet} inntektsPeriode={inntektsPeriode}/>
+                <Button icon={<PlusCircleIcon/>} size="small" className="mt-4">
+                    Legg til inntekt
+                </Button>
+                <Button icon={<TrashIcon/>} variant="tertiary" size="small" className="mt-4" onClick={() => slettVirksomhet(virksomhetsnummer)}>
+                    Slett inntekt
+                </Button>
+            </ExpansionCard.Content>
+        </ExpansionCard>
+    );
 }
--- app/components/Virksomheter.tsx
+++ app/components/Virksomheter.tsx
@@ -1,10 +1,22 @@
 import { useInntekt } from "~/context/inntekt-context";
 import Virksomhet from "./Virksomhet";
 import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
+import { useEffect } from "react";
 
 export function Virksomheter() {
   const { periode } = useTypedRouteLoaderData("routes/inntektId.$inntektId");
-  const { contextVirksomheter } = useInntekt();
+  const { contextVirksomheter, setContextViksomheter, setInntektEndret } = useInntekt();
+
+  function slettVirksomhetInntekt (virksomhetsnummer: string) {
+    setInntektEndret(true)
+    var oppdatertKontekstVirksomheter = contextVirksomheter.filter(virksomhet => virksomhet.virksomhetsnummer !== virksomhetsnummer)
+    setContextViksomheter(oppdatertKontekstVirksomheter)
+  }
+
+  useEffect(() => {
+    console.log("UseEffect", contextVirksomheter)
+  }, [contextVirksomheter])
+
 
   return (
     <>
@@ -13,6 +25,7 @@ export function Virksomheter() {
           key={virksomhet.virksomhetsnummer}
           virksomhet={virksomhet}
           inntektsPeriode={periode}
+          slettVirksomhet={slettVirksomhetInntekt}
         />
       ))}
     </>
