617b32116037c891d20c59873496f8422f590a7c
ArselanSultani <arselan.sultani@nav.no>
2025-06-10 16:24:41 +0200

    Slett inntekttyper
    * Slett inntekttype for virksomhet
    
    * Slett virksomheten hvis man sletter den siste inntekttypen

--- app/components/VirksomhetInntekter.tsx
+++ app/components/VirksomhetInntekter.tsx
@@ -1,5 +1,5 @@
-import { Button, Table } from "@navikt/ds-react";
-import type { IPeriode, IVirksomhet } from "~/types/inntekt.types";
+import { Button, HStack, Table } from "@navikt/ds-react";
+import type { IPeriode, IUklassifisertInntekt, IVirksomhet } from "~/types/inntekt.types";
 import { inntektTyperBeskrivelse } from "~/utils/constants";
 import { formatterNorskTall } from "~/utils/formattering.util";
 import {
@@ -7,19 +7,61 @@ import {
   delOppPeriodeTilTrePerioder,
   grupperEtterInntektType,
 } from "~/utils/inntekt.util";
-import { VirsomhetPeriodeHeader } from "./VirsomhetPeriodeHeader";
+import { VirksomhetPeriodeHeader } from "./VirksomhetPeriodeHeader";
+import { NotePencilIcon, TrashIcon } from "@navikt/aksel-icons";
+import { useInntekt } from "~/context/inntekt-context";
 
 interface IProps {
   virksomhet: IVirksomhet;
   inntektsPeriode: IPeriode;
 }
 
-export default function VirsomhetInntekter({ virksomhet, inntektsPeriode }: IProps) {
+export default function VirksomhetInntekter({ virksomhet, inntektsPeriode }: IProps) {
   const gruppertinntektTyperBeskrivelse = grupperEtterInntektType(virksomhet.inntekter);
   const oppdeltPerioder = delOppPeriodeTilTrePerioder(inntektsPeriode);
   const periode1 = oppdeltPerioder[0];
   const periode2 = oppdeltPerioder[1];
   const periode3 = oppdeltPerioder[2];
+  const { uklassifisertInntekt, setUklassifisertInntekt, setInntektEndret } = useInntekt();
+
+  function fjernInntekt(inntektType: string) {
+    const oppdatertInntektForGittVirksomhet = virksomhet.inntekter.filter(
+      (inntekt) => inntekt.inntektType !== inntektType
+    );
+
+    const oppdatertVirksomhet: IVirksomhet = {
+      ...virksomhet,
+      inntekter: oppdatertInntektForGittVirksomhet,
+    };
+
+    const oppdatertVirksomheter = [
+      ...uklassifisertInntekt.virksomheter.filter(
+        (v) => v.virksomhetsnummer !== virksomhet.virksomhetsnummer
+      ),
+      oppdatertVirksomhet,
+    ];
+
+    const oppdatertUklassifisertInntekt: IUklassifisertInntekt = {
+      ...uklassifisertInntekt,
+      virksomheter: oppdatertVirksomheter,
+    };
+
+    setUklassifisertInntekt(oppdatertUklassifisertInntekt);
+    setInntektEndret(true);
+  }
+
+  function slettVirksomhet() {
+    var oppdatertKontekstVirksomheter = uklassifisertInntekt.virksomheter.filter(
+      (v) => virksomhet.virksomhetsnummer !== v.virksomhetsnummer
+    );
+
+    setUklassifisertInntekt({
+      ...uklassifisertInntekt,
+      virksomheter: oppdatertKontekstVirksomheter,
+    });
+
+    setInntektEndret(true);
+  }
 
   return (
     <Table>
@@ -28,13 +70,13 @@ export default function VirsomhetInntekter({ virksomhet, inntektsPeriode }: IPro
           <Table.HeaderCell scope="col">Inntektstype</Table.HeaderCell>
           <Table.HeaderCell scope="col">Kilde</Table.HeaderCell>
           <Table.HeaderCell scope="col" align="right">
-            <VirsomhetPeriodeHeader periodeNummer={1} periode={periode1} />
+            <VirksomhetPeriodeHeader periodeNummer={1} periode={periode1} />
           </Table.HeaderCell>
           <Table.HeaderCell scope="col" align="right">
-            <VirsomhetPeriodeHeader periodeNummer={2} periode={periode2} />
+            <VirksomhetPeriodeHeader periodeNummer={2} periode={periode2} />
           </Table.HeaderCell>
           <Table.HeaderCell scope="col" align="right">
-            <VirsomhetPeriodeHeader periodeNummer={3} periode={periode3} />
+            <VirksomhetPeriodeHeader periodeNummer={3} periode={periode3} />
           </Table.HeaderCell>
           <Table.HeaderCell scope="col"></Table.HeaderCell>
         </Table.Row>
@@ -57,9 +99,19 @@ export default function VirsomhetInntekter({ virksomhet, inntektsPeriode }: IPro
               {formatterNorskTall(beregnTotalInntektForEnPeriode(inntekt.inntekter, periode3))}
             </Table.DataCell>
             <Table.DataCell align="right">
-              <Button variant="tertiary" size="small">
-                Rediger
-              </Button>
+              <HStack gap="1" justify="end">
+                <Button variant="tertiary" size="small" icon={<NotePencilIcon />} />
+                <Button
+                  variant="tertiary"
+                  size="small"
+                  icon={<TrashIcon />}
+                  onClick={() =>
+                    gruppertinntektTyperBeskrivelse.length > 1
+                      ? fjernInntekt(inntekt.inntektType)
+                      : slettVirksomhet()
+                  }
+                />
+              </HStack>
             </Table.DataCell>
           </Table.Row>
         ))}
