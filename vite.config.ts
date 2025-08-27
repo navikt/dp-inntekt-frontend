79e9a3977c4295ceebd1d64cd3abf514aee76b05
ArselanSultani <arselan.sultani@nav.no>
2025-08-27 12:08:15 +0200

    Deploy til prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { reactRouter } from "@react-router/dev/vite";
+import {reactRouter} from "@react-router/dev/vite";
 import path from "path";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "https://cdn.nav.no/teamdagpenger/dp-inntekt-frontend/client/"
+      : "/",
   plugins: [reactRouter(), tsconfigPaths(), devtoolsJson()],
   build: {
     manifest: true,
