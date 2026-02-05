73a8bf4cc7cfd2f1f6dd28fe9b1ac8c1965696c1
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-02-05 13:38:57 +0100

    Legge til funksjonalitet for å kunne vise eller skjule sensitive opplysninger

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,31 @@
-import { BodyShort, Box, HStack } from "@navikt/ds-react";
+import { BodyShort, Box, Button, HStack } from "@navikt/ds-react";
 import { NavLogoIkon } from "./Ikoner/NavLogoIkon";
+import { EyeSlashIcon, EyeWithPupilIcon } from "@navikt/aksel-icons";
+import { useInntekt } from "~/context/inntekt-context";
 
 interface IProps {
   tittel: string;
 }
 
 export function Header({ tittel }: IProps) {
+  const { setSkjulSensitiveOpplysninger, skjulSensitiveOpplysninger } = useInntekt();
+
   return (
     <Box background="surface-default" padding="6" borderRadius="xlarge" borderColor="border-subtle">
-      <HStack gap="4">
-        <NavLogoIkon /> <BodyShort weight="semibold">{tittel}</BodyShort>
+      <HStack gap="4" justify="space-between" align="center">
+        <HStack gap="4">
+          <NavLogoIkon /> <BodyShort weight="semibold">{tittel}</BodyShort>
+        </HStack>
+        <Button
+          size="small"
+          variant="secondary"
+          icon={skjulSensitiveOpplysninger ? <EyeSlashIcon /> : <EyeWithPupilIcon />}
+          onClick={() => setSkjulSensitiveOpplysninger(!skjulSensitiveOpplysninger)}
+        >
+          {skjulSensitiveOpplysninger
+            ? "Vis sensitive opplysninger"
+            : "Skjul sensitive opplysninger"}
+        </Button>
       </HStack>
     </Box>
   );
