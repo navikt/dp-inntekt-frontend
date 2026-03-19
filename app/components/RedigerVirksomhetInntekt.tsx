8bd12ed7ab34b954e1a333930de382016cac3787
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-03-19 14:00:15 +0100

    Ikke disable rediger knappen for se på inntekter, rediger modal lagringsknapp og disable inputs

--- app/components/RedigerVirksomhetInntekt.tsx
+++ app/components/RedigerVirksomhetInntekt.tsx
@@ -17,7 +17,6 @@ interface IProps {
 
 export function RedigerVirksomhetInntekt({ virksomhet, formDefaultValues }: IProps) {
   const ref = useRef<HTMLDialogElement>(null);
-  const { readOnly, inntektLagret } = useInntektSeachParams();
 
   return (
     <>
@@ -26,7 +25,6 @@ export function RedigerVirksomhetInntekt({ virksomhet, formDefaultValues }: IPro
         size="small"
         icon={<NotePencilIcon />}
         onClick={() => ref.current?.showModal()}
-        disabled={readOnly || inntektLagret}
       />
       <RedigerModal ref={ref} virksomhet={virksomhet} formDefaultValues={formDefaultValues} />
     </>
