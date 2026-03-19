11fd6d7a72c7d8cd32a6eb82d1a7db0ba5bb7d65
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-03-19 13:44:00 +0100

    Legge til conditional rendering for Lesevisning badge

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 import { BodyShort, Box, HStack, Switch, Tag } from "@navikt/ds-react";
 import { useInntekt } from "~/context/inntekt-context";
 import { NavLogoIkon } from "./Ikoner/NavLogoIkon";
+import { useInntektSeachParams } from "~/hooks/useInntektSeachParams";
 
 interface IProps {
   tittel: string;
@@ -8,15 +9,18 @@ interface IProps {
 
 export function Header({ tittel }: IProps) {
   const { setSkjulSensitiveOpplysninger, skjulSensitiveOpplysninger } = useInntekt();
+  const { readOnly } = useInntektSeachParams();
 
   return (
     <Box background="surface-default" padding="6" borderRadius="xlarge" borderColor="border-subtle">
       <HStack gap="4" justify="space-between" align="center">
         <HStack gap="4">
           <NavLogoIkon /> <BodyShort weight="semibold">{tittel}</BodyShort>
-          <Tag variant="info" data-color="info" size="small">
-            Lesevisning
-          </Tag>
+          {readOnly && (
+            <Tag variant="info" data-color="info" size="small">
+              Lesevisning
+            </Tag>
+          )}
         </HStack>
 
         <HStack gap="4">
