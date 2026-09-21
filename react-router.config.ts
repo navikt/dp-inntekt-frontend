4d6aa8a5c8fe193074acdf58bbb21ae145e23170
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-09-21 08:34:30 +0200

    Bumpe patches og minor, får sw bug

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  future: {
+    v8_middleware: false,
+    v8_splitRouteModules: false,
+    v8_viteEnvironmentApi: false,
+    v8_passThroughRequests: false,
+    v8_trailingSlashAwareDataRequests: false
+  },
   ssr: true,
 } satisfies Config;
