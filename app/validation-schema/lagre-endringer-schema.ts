8ceb24d6d3fea8330ebb5f26a9c7641028fafab3
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-09-22 14:10:08 +0200

    Revert "Bumpe pakker (#132)"
    This reverts commit 2585b4713e93e07d03979f00d16d15e825bd92c0.

--- app/validation-schema/lagre-endringer-schema.ts
+++ app/validation-schema/lagre-endringer-schema.ts
@@ -3,21 +3,21 @@ import { z } from "zod";
 export const lagreEndringerSchema = z.object({
   payload: z
     .string({
-      error: "Payload er påkrevd",
+      required_error: "Payload er påkrevd",
     })
     .optional(),
   inntektId: z.string({
-    error: "InntektId er påkrevd",
+    required_error: "InntektId er påkrevd",
   }),
   behandlingId: z.string({
-    error: "BehandlingId er påkrevd",
+    required_error: "BehandlingId er påkrevd",
   }).optional(),
   opplysningId: z.string({
-    error: "OpplysningId er påkrevd",
+    required_error: "OpplysningId er påkrevd",
   }).optional(),
   begrunnelse: z
     .string({
-      error: "Begrunnelse er påkrevd",
+      required_error: "Begrunnelse er påkrevd",
     })
     .refine((val) => /[a-zA-ZæøåÆØÅ]/.test(val ?? ""), {
       message: "Begrunnelse må inneholde minst én bokstav",
